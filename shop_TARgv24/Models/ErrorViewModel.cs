e59a0975d6fa53ed5e49b7bcc1c58e8c9e1265e1
Nkk15-21 <nkk.sith@gmail.com>
2025-09-02 15:23:55 +0300

    Esimene tegevus

---
+++ shop_TARgv24/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace shop_TARgv24.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
