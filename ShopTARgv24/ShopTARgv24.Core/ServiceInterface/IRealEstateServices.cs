01c48bc93c3444a6f1a25b2f31e690800de6fdc4
Nkk15-21 <nkk.sith@gmail.com>
2025-11-11 13:57:50 +0200

    ServiceInterface

--- ShopTARgv24/ShopTARgv24.Core/ServiceInterface/IRealEstateServices.cs
+++ ShopTARgv24/ShopTARgv24.Core/ServiceInterface/IRealEstateServices.cs
@@ -6,9 +6,8 @@ namespace ShopTARgv24.Core.ServiceInterface
     public interface IRealEstateServices
     {
         Task<RealEstate> Create(RealEstateDto dto);
-        Task<RealEstate> DetailAsync(Guid id);
         Task<RealEstate> Update(RealEstateDto dto);
+        Task<RealEstate> DetailAsync(Guid id);
         Task<RealEstate> Delete(Guid id);
-
     }
 }
