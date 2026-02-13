9a1dfa58279cf2e095b3620065bca26efed6ee9e
Ваше имя < email>
2026-02-14 00:35:54 +0200

    Kindergarten ServiceInterface

--- ShopTARgv24/ShopTARgv24.Core/ServiceInterface/FileServices.cs
+++ ShopTARgv24/ShopTARgv24.Core/ServiceInterface/FileServices.cs
@@ -1,10 +1,14 @@
 ﻿using ShopTARgv24.Core.Domain;
 using ShopTARgv24.Core.Dto;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace ShopTARgv24.Core.ServiceInterface
 {
     public interface IFileServices
     {
-        void FilesToApi(SpaceshipDto dto, Spaceship spaceship);
+        Task<FileToDatabase> RemoveImageFromDatabase(FileToDatabaseDto dto);
+        Task<FileToDatabase> RemoveImagesFromDatabase(FileToDatabaseDto[] dtos);
+        void UploadFilesToDatabase(KindergartenDto dto, Kindergarten domain);
     }
 }
