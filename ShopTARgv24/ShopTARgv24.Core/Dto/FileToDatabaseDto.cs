560a958d2fe4d9d0179b6c9bc25da835a998b0fc
Ваше имя < email>
2026-02-14 00:34:18 +0200

    Kindergarten DTO

---
+++ ShopTARgv24/ShopTARgv24.Core/Dto/FileToDatabaseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ShopTARgv24.Core.Dto;
+
+public class FileToDatabaseDto
+{
+    public Guid Id { get; set; }
+    public string? ImageTitle { get; set; }
+    public byte[]? ImageData { get; set; }
+    public Guid? KindergartenId { get; set; }
+
+}
